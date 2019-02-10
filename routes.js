0b0a35cb54beef73fd5702920e98c364c6f5fe1b
Moimolcp <moimolcp@gmail.com>
2019-02-10 16:12:36 -0500

    locura

--- routes.js
+++ routes.js
@@ -1,16 +1,25 @@
 const mysql = require('mysql');
-const connection = mysql.createConnection({
+const connectionW = mysql.createConnection({
+    host: process.env.MYSQL_HOST || 'lb-reviews-sql',
+    user: process.env.MYSQL_USER || 'root',
+    port: '6031',
+    password: process.env.MYSQL_PASSWORD || 'reviewPassword',
+    database: process.env.MYSQL_DATABASE || 'review_suggestions'
+});
+
+const connectionR = mysql.createConnection({
     host: process.env.MYSQL_HOST || 'lb-reviews-sql',
     user: process.env.MYSQL_USER || 'root',
     port: '6032',
     password: process.env.MYSQL_PASSWORD || 'reviewPassword',
     database: process.env.MYSQL_DATABASE || 'review_suggestions'
 });
+
 module.exports = function (app) {
     // get all reviews
     app.get('/reviews', (req, res) => {
         const query = 'SELECT * FROM reviews';
-        connection.query(query, (err, results, fields) => {
+        connectionR.query(query, (err, results, fields) => {
             if (err) {
                 console.error(err);
                 res.json({
@@ -28,7 +37,7 @@ module.exports = function (app) {
     app.get('/reviews/:id', (req, res) => {
         
         const query = 'SELECT * FROM reviews where review_id = ' + req.params.id;
-        connection.query(query, (err, results, fields) => {
+        connectionR.query(query, (err, results, fields) => {
             if (isNaN(req.params.id)) {
                 res.status(400).json({ message: "id is not valid" });
             } else if (Object.keys(results).length == 0) {
@@ -49,7 +58,7 @@ module.exports = function (app) {
     // get all suggestions
     app.get('/suggestions', (req, res) => {
         const query = 'SELECT * FROM suggestions';
-        connection.query(query, (err, results, fields) => {
+        connectionR.query(query, (err, results, fields) => {
             if (err) {
                 console.error(err);
                 res.json({
@@ -67,7 +76,7 @@ module.exports = function (app) {
     app.get('/suggestions/:id', (req, res) => {
         
         const query = 'SELECT * FROM suggestions where suggestion_id = ' + req.params.id;
-        connection.query(query, (err, results, fields) => {
+        connectionR.query(query, (err, results, fields) => {
             if (isNaN(req.params.id)) {
                 res.status(400).json({ message: "id is not valid" });
             } else if (Object.keys(results).length == 0) {
@@ -89,7 +98,7 @@ module.exports = function (app) {
     app.get('/get-average/:book_id', (req, res) => {
         
         const query = 'SELECT AVG(grade) as Avg FROM reviews where book_id = ' + req.params.book_id;
-        connection.query(query, (err, results, fields) => {
+        connectionR.query(query, (err, results, fields) => {
             if (isNaN(req.params.book_id)) {
                 res.status(400).json({ message: "id is not valid" });
             } else if (results[0].Avg == null) {
@@ -114,7 +123,7 @@ module.exports = function (app) {
     app.get('/get-reviews/:book_id', (req, res) => {
         
         const query = 'SELECT * FROM reviews where book_id = ' + req.params.book_id;
-        connection.query(query, (err, results, fields) => {
+        connectionR.query(query, (err, results, fields) => {
             if (isNaN(req.params.book_id)) {
                 res.status(400).json({ message: "id is not valid" });
             } else if (Object.keys(results).length == 0) {
@@ -138,7 +147,7 @@ module.exports = function (app) {
      // get all reviews by user
      app.get('/get-reviewsUser/:user_id', (req, res) => {       
         const query = 'SELECT * FROM reviews where user_id = ' + req.params.user_id;
-        connection.query(query, (err, results, fields) => {
+        connectionR.query(query, (err, results, fields) => {
             if (isNaN(req.params.user_id)) {
                 res.status(400).json({ message: "id is not valid" });
             } else if (Object.keys(results).length == 0) {
@@ -162,7 +171,7 @@ module.exports = function (app) {
     app.get('/get-suggestions/:book_id', (req, res) => {
         
         const query = 'SELECT * FROM suggestions where book_id1 = ' + req.params.book_id ;
-        connection.query(query, (err, results, fields) => {
+        connectionR.query(query, (err, results, fields) => {
             if (isNaN(req.params.book_id)) {
                 res.status(400).json({ message: "id is not valid" });
             } else if (Object.keys(results).length == 0) {
@@ -187,7 +196,7 @@ module.exports = function (app) {
     app.get('/get-suggestionsUser/:user_id', (req, res) => {
         
         const query = 'SELECT * FROM suggestions where user_id = ' + req.params.user_id ;
-        connection.query(query, (err, results, fields) => {
+        connectionR.query(query, (err, results, fields) => {
             if (isNaN(req.params.user_id)) {
                 res.status(400).json({ message: "id is not valid" });
             } else if (Object.keys(results).length == 0) {
@@ -213,7 +222,7 @@ module.exports = function (app) {
         const review = req.body;
         const query = 'INSERT INTO reviews(book_id,booktitle,user_id,username,review,grade) values(?, ?, ?, ?, ?, ?)';
         
-        connection.query(query, [review.book_id, review.booktitle, review.user_id, review.username, review.review, review.grade], (err, results, fields) => {
+        connectionW.query(query, [review.book_id, review.booktitle, review.user_id, review.username, review.review, review.grade], (err, results, fields) => {
             if (err) {
                 console.error("errno=" + err.errno);
                 if (err.errno == 1366) { //BAD REQUEST
@@ -237,7 +246,7 @@ module.exports = function (app) {
     app.post('/suggestions', (req, res) => {
         const suggestion = req.body;
         const query = 'INSERT INTO suggestions(user_id,username,book_id1,booktitle1,book_id2,booktitle2,reason) values(?, ?, ?, ?, ?, ?, ?),(?, ?, ?, ?, ?, ?, ?)';
-        connection.query(query, [suggestion.user_id, suggestion.username, suggestion.book_id1, suggestion.booktitle1, suggestion.book_id2, suggestion.booktitle2, suggestion.reason, suggestion.user_id, suggestion.username, suggestion.book_id2, suggestion.booktitle2, suggestion.book_id1, suggestion.booktitle1, suggestion.reason], (err, results, fields) => {
+        connectionW.query(query, [suggestion.user_id, suggestion.username, suggestion.book_id1, suggestion.booktitle1, suggestion.book_id2, suggestion.booktitle2, suggestion.reason, suggestion.user_id, suggestion.username, suggestion.book_id2, suggestion.booktitle2, suggestion.book_id1, suggestion.booktitle1, suggestion.reason], (err, results, fields) => {
             if (err) {
                 console.error("errno=" + err.errno);
                 if (err.errno == 1366) { //BAD REQUEST
@@ -262,7 +271,7 @@ module.exports = function (app) {
         const review = req.body;
         const queryupdate = 'update reviews set book_id = ? , booktitle = ?, user_id = ?, username = ? ,review = ? ,grade= ? where review_id ='+req.params.review_id;
         
-        connection.query(queryupdate, [review.book_id, review.booktitle, review.user_id, review.username, review.review, review.grade], (err, results, fields) => {
+        connectionW.query(queryupdate, [review.book_id, review.booktitle, review.user_id, review.username, review.review, review.grade], (err, results, fields) => {
             if (results != undefined && results.affectedRows == 0) {
                 res.status(404).json({ message: "Review with id = " + req.params.review_id + " doesn't exists" });
             } else if (err) {
@@ -291,7 +300,7 @@ module.exports = function (app) {
         //const queryupdate2 = 'update suggestions set book_id2 = ? ,book_id1 = ? ,reason = ? where suggestion_id ='+req.params.suggestion_id;
         //PENSAR EN UPDATE SUGGESTION DOBLE
         
-        connection.query(queryupdate1, [suggestion.user_id, suggestion.username, suggestion.book_id1, suggestion.booktitle1, suggestion.book_id2, suggestion.booktitle2, suggestion.reason], (err, results, fields) => {
+        connectionW.query(queryupdate1, [suggestion.user_id, suggestion.username, suggestion.book_id1, suggestion.booktitle1, suggestion.book_id2, suggestion.booktitle2, suggestion.reason], (err, results, fields) => {
             if (results != undefined && results.affectedRows == 0) {
                 res.status(404).json({ message: "Suggestion with id = " + req.params.suggestion_id + " doesn't exists" });
             } else if (err) {
@@ -318,7 +327,7 @@ module.exports = function (app) {
         const body = req.body;
         const query = 'delete from reviews where review_id ='+req.params.review_id;
         
-        connection.query(query,[], (err, results, fields) => {
+        connectionW.query(query,[], (err, results, fields) => {
             if (isNaN(req.params.review_id)) {
                 res.status(400).json({ message: "id is not valid" });
             } else if (results != undefined && results.affectedRows == 0) {
@@ -341,7 +350,7 @@ module.exports = function (app) {
         const body = req.body;
         const query = 'delete from suggestions where suggestion_id ='+req.params.suggestion_id;
         
-        connection.query(query, [], (err, results, fields) => {
+        connectionW.query(query, [], (err, results, fields) => {
             if (isNaN(req.params.suggestion_id)) {
                 res.status(400).json({ message: "id is not valid" });
             } else if (results != undefined && results.affectedRows == 0) {
